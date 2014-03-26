6d3d74f34128189787a994ec7ad119cb7ca0264a
Viswanathan R <vichu.wizard@gmail.com>
2014-03-26 01:01:26 -0500

    Added RotateImage.java

--- RotateImage.java
+++ RotateImage.java
@@ -30,7 +30,7 @@ public class RotateImage {
                                 {1,2,3,4},
                                 {5,6,7,8},
                                 {9,10,11,12},
-                            {13,14,15,16,} };
+                            {13,14,15,16} };
 
         int[][] result = rotMat.rotateMatrix(imageMatrix);                        
         for(int k=0;k<imageMatrix.length;k++){
