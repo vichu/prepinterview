0c8440602fc4ce2a12c15733e183510ebdd12b9b
Viswanathan R <vichu.wizard@gmail.com>
2014-04-02 00:29:37 -0500

    Added the rotate matrix implementation while keeping the array in-place

--- RotateImage.java
+++ RotateImage.java
@@ -24,6 +24,28 @@ public class RotateImage {
         return resultMatrix; 
     }
     
+    public int[][] rotateInPlace(int[][] image, int n) {
+        
+        for (int layer=0; layer<n/2; layer++) {
+            int first = layer;
+            int last = n-layer-1;
+            int offset = 0;
+            for (int i=first; i<last; i++) {
+                
+                int top = image[first][i];
+                image[first][i] = image[last-offset][first];
+                image[last-offset][first] = image[last][last-offset];
+                image[last][last-offset] = image[i][last];
+                image[i][last] = top;
+                offset++;
+            }
+        }
+        
+        return image;
+        
+    }
+    
+    
     public static void main(String[] args) {
         RotateImage rotMat = new RotateImage();
         int[][] imageMatrix = new int[][]{ 
@@ -37,6 +59,16 @@ public class RotateImage {
             for(int l=0;l<imageMatrix.length;l++)
             {
                 System.out.print(" "+result[k][l]);
+            } 
+            System.out.println("" );
+        }
+        
+        System.out.println("");
+        int[][] resultinPlace = rotMat.rotateInPlace(imageMatrix, 4);
+        for(int k=0;k<imageMatrix.length;k++){
+            for(int l=0;l<imageMatrix.length;l++)
+            {
+                System.out.print(" "+resultinPlace[k][l]);
             } 
             System.out.println("" );
         }                       
